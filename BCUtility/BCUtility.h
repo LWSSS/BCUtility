6bea11e5180ab71940874b8acd20367e8395ff25
刘委 <liuwei_local@126.com>
2024-06-18 19:35:24 +0800

    Initial Commit

---
+++ BCUtility/BCUtility.h
@@ -0,0 +1,18 @@
+//
+//  BCUtility.h
+//  BCUtility
+//
+//  Created by 易点云 on 2024/6/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BCUtility.
+FOUNDATION_EXPORT double BCUtilityVersionNumber;
+
+//! Project version string for BCUtility.
+FOUNDATION_EXPORT const unsigned char BCUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BCUtility/PublicHeader.h>
+
+
